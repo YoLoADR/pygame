af1e25974419830b72cf40e8cca7a12f583eb969
YoLoADR <YohannPro@macbook-air-de-yohann.home>
2019-08-01 04:28:53 +0200

    Adding player and set up background

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
+# Game lopp
 running = True
 
 while running:
+    # Background
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    player()
+    pygame.display.update()
