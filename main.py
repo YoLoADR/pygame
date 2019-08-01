f9ce22f47e896075c92e9bd5d171725b8878b372
YoLoADR <YohannPro@macbook-air-de-yohann.home>
2019-08-01 05:02:02 +0200

    set up player mouvement

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # Game lopp
@@ -30,5 +32,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player()
+        # if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left arrow is pressed")
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                print("Right arrow is pressed")
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                print("Up arrow is pressed")
+                playerY_change = -0.3
+            if event.key == pygame.K_DOWN:
+                print("Down arrow is pressed")
+                playerY_change = 0.3
+    playerX += playerX_change
+    playerY += playerY_change
+    player(playerX, playerY)
     pygame.display.update()
